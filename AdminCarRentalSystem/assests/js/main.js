a9cf7b351d3367fe5da924356d1c3cd2c3253597
parindyawijeseskara <parindyawijesekara@gmail.com>
2022-07-22 23:31:08 +0530

    Add Admin project structure

---
+++ AdminCarRentalSystem/assests/js/main.js
@@ -0,0 +1,10 @@
+
+function manageCars() {
+    $('.main-dashboard-container>section').css({display: "none"});
+    $('.cars-container').css({display: "block"});
+}
+
+function navigateTODashboard() {
+    $('.main-dashboard-container>section').css({display: "none"});
+    $('.admin-dashboard-container').css({display: "block"});
+}
